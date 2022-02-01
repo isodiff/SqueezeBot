08eef79fd2c1574b0c5d2255670cc04bf74905a8
isodiff <71041838+isodiff@users.noreply.github.com>
2022-02-01 19:18:11 +0100

    initalize Github repository

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+const TOKEN = "OTM4MTIxMzcxNDg0MDYxNzY2.YflrnA.rtBCUrHXkyJg3FvVatLSX47bucQ"
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi") {
+        message.reply("Hello World!")
+    }
+})
+
+client.login(process.env.TOKEN)
