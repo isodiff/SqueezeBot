44eea6d55352d58ebfa0a64f3158b5182d1e7a43
isodiff <71041838+isodiff@users.noreply.github.com>
2022-02-02 12:00:59 +0100

    added slash commands

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ let bot = {
 
 client.commands = new Discord.Collection()
 client.events = new Discord.Collection()
-
+client.slashcommands = new Discord.Collection()
 
 client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
 client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
 
 client.loadEvents(bot, false)
 client.loadCommands(bot, false)
+client.loadSlashCommands(bot, false)
 
 module.exports = bot
 
@@ -48,6 +50,5 @@ module.exports = bot
 //         files: [image]
 //     })
 // })
-
 
 client.login(process.env.TOKEN)
