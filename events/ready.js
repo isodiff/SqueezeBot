164f4763169259724bc828c057c73c6aada28490
Isodiff <bogdan.cimoch@outlook.com>
2022-02-04 22:08:35 +0100

    added help command

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,12 @@ module.exports = {
     name: "ready",
     run: async (bot) => {
         console.log("Logged in as " + bot.client.user.tag)
-        bot.client.user.setActivity("you suffer", { type: "WATCHING" })
+        // bot.client.user.setActivity("you suffer", { type: "WATCHING" })
+        bot.client.user.setPresence({
+            game: {
+                name: 'Type !help',
+                type: 0
+            }
+        });
     }
 }
--- slashcommands/bot-info.js
+++ slashcommands/bot-info.js
@@ -4,61 +4,76 @@ const run = async (client, interaction) => {
     const discordJSVersion = packageJSON.dependencies["discord.js"];
     const os = require("os");
 
+    function between(min, max) {
+        return Math.floor(
+            Math.random() * (max - min) + min
+        )
+    }
+
+
     const freeMemory = os.freemem() / 1000000000;
     const totalMemory = os.totalmem() / 1000000000;
 
-    const infoEmbed = {
-        color: "RANDOM",
-        title: 'Bot Stats',
-        author: {
-            name: 'author: twfl',
-            icon_url: 'https://de.catbox.moe/65u3r5.jpg',
-            url: 'https://github.com/isodiff',
-        },
-        description: 'Blahaj Maid Discord bot. Cleanse the corrupt souls of this cursed land, and more!',
-        fields: [
-            {
-                name: "Uptime:",
-                value: `${prettyMilliseconds(client.uptime)}`,
-                inline: false
-            }, {
-                name: 'Username:',
-                value: `${client.user.tag}`,
-                inline: true
-            }, {
-                name: 'Commands:',
-                value: `9`,
-                inline: true
-            }, {
-                name: 'Prefix:',
-                value: `\! or \/`,
-                inline: true
-            }, {
-                name: 'discord.js:',
-                value: `${discordJSVersion}`,
-                inline: true
-            }, {
-                name: 'Total memory (GB):',
-                value: `${totalMemory}`,
-                inline: true
-            }, {
-                name: 'Free memory (GB):',
-                value: `${freeMemory}`,
-                inline: true
-            }, {
-                name: 'Source Code:',
-                value: '[Click here](https://github.com/isodiff/SqueezeBot)',
-                inline: true
-            }, {
-                name: 'Documentation:',
-                value: '[Click here](https://github.com/isodiff/SqueezeBot/tree/main#readme)',
-                inline: true
-            }
-        ],
-        timestamp: new Date(),
-    }
+
 
     try {
+
+        const infoEmbed = {
+            color: "RANDOM",
+            title: 'Bot Stats',
+            author: {
+                name: 'author: twfl',
+                icon_url: 'https://de.catbox.moe/65u3r5.jpg',
+                url: 'https://github.com/isodiff',
+            },
+            description: 'Blahaj Maid Discord bot. Cleanse the corrupt souls of this cursed land, and more!',
+            fields: [
+                {
+                    name: "Uptime:",
+                    value: `${prettyMilliseconds(client.uptime)}`,
+                    inline: false
+                }, {
+                    name: 'Username:',
+                    value: `${client.user.tag}`,
+                    inline: true
+                }, {
+                    name: 'Commands:',
+                    value: `9`,
+                    inline: true
+                }, {
+                    name: 'Prefix:',
+                    value: `\! or \/`,
+                    inline: true
+                }, {
+                    name: 'discord.js:',
+                    value: `${discordJSVersion}`,
+                    inline: true
+                }, {
+                    name: 'Total memory (GB):',
+                    value: `${totalMemory}`,
+                    inline: true
+                }, {
+                    name: 'Free memory (GB):',
+                    value: `${freeMemory}`,
+                    inline: true
+                }, {
+                    name: 'Source Code:',
+                    value: '[Click here](https://github.com/isodiff/SqueezeBot)',
+                    inline: true
+                }, {
+                    name: 'Documentation:',
+                    value: '[Click here](https://github.com/isodiff/SqueezeBot#squeezebot-also-known-as-blahaj-maid)',
+                    inline: true
+                },
+                {
+                    name: 'Random number (1 or 2):',
+                    value: `${between(1, 3)}`,
+                    inline: true
+                }
+            ],
+            timestamp: new Date(),
+        }
+
         await interaction.reply({ embeds: [infoEmbed] })
         return
     }
