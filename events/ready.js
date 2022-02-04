7dc07fbf6156d1c6b44da2cfef1f08a96e987f32
Isodiff <bogdan.cimoch@outlook.com>
2022-02-04 23:54:41 +0100

    added more logs

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,12 @@ module.exports = {
     name: "ready",
     run: async (bot) => {
         const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+
         const guildsId = bot.client.guilds.cache.map(guild => guild.id);
         const guildsNames = bot.client.guilds.cache.map(guild => guild.name);
+
         const guilds = zip(guildsId, guildsNames)
+
         console.log(guilds);
         console.log(`Logged in as ${bot.client.user.tag} in ${guilds.length} guilds`)
 
